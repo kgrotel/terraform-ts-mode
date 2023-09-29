48394b7062cba0b36947db7026dcdab18ec8021e
Kai Grotelüschen <kgr@ccbash.de>
2023-09-29 11:10:30 +0200

    fixing headers

--- terraform-ts-mode.el
+++ terraform-ts-mode.el
@@ -1,10 +1,35 @@
-;;; my-cabal-mode.el --- My Cabal mode -*- lexical-binding: t -*-
+;;; terraform-ts-mode.el --- Terraform major mode using Treesitter and eglot  -*- lexical-binding: t -*-
+
+;;; Copyright (C) 2022-2027 Kai Grotelüschen
+
+;; Author:     Kai Grotelüschen <kgr@gnotes.de>
+;; Maintainer: Kai Grotelüschen <kgr@gnotes.de>
+;; Version:    0.4
+;; Keywords:   elisp, extensions
+;; Homepage:   https://github.com/kgrotel/terraform-ts-mode
+;; Package-Requires: ((emacs "29.1"))
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses.
+
+
 ;;; Commentary:
+
+;; This is a terraform mode using treesit. There are still quite some
+;; Isues with using Treesitter for imenu and Highlight so any kind of
+;; help is greatly appreaciated
+
 ;;; Code:
-
-;; TODO
-;; - format, run terraform-fmt on buffer
-
 
 (require 'treesit)
 (require 'eglot)
